6835ece845f69e275913c9d504aab1351dd11fca
def750 <def75023@gmail.com>
2023-05-06 18:20:50 +0000

    Structs

---
+++ src/structures/mods.rs
@@ -0,0 +1,41 @@
+use bitflags::bitflags;
+use serde::Deserialize;
+
+// Create a bitflags struct
+bitflags! {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    pub struct Mods: u32 {
+        NoMod = 0;
+        NoFail = 1 << 0;
+        Easy = 1 << 1;
+        TouchScreen = 1 << 2;  // old: 'NOVIDEO'
+        Hidden = 1 << 3;
+        HardRock = 1 << 4;
+        SuddenDeath = 1 << 5;
+        DoubleTime = 1 << 6;
+        Relax = 1 << 7;
+        HalfTime = 1 << 8;
+        NightCore = 1 << 9;
+        FlashLight = 1 << 10;
+        AutoPlay = 1 << 11;
+        SpunOut = 1 << 12;
+        AutoPilot = 1 << 13;
+        Perfect = 1 << 14;
+        Key4 = 1 << 15;
+        Key5 = 1 << 16;
+        Key6 = 1 << 17;
+        Key7 = 1 << 18;
+        Key8 = 1 << 19;
+        FadeIn = 1 << 20;
+        Random = 1 << 21;
+        Cinema = 1 << 22;
+        Target = 1 << 23;
+        Key9 = 1 << 24;
+        KeyCOOP = 1 << 25;
+        Key1 = 1 << 26;
+        Key3 = 1 << 27;
+        Key2 = 1 << 28;
+        ScoreV2 = 1 << 29;
+        Mirror = 1 << 30;
+    }
+}

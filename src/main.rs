4376d867e699ff5a39b9656f1378ffe9e1dafbb8
def750 <def75023@gmail.com>
2023-05-13 10:45:09 +0000

    Stuff in main (pending cleanup)

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,105 @@
 mod config;
 mod structures;
 
-use akatsuki_pp::{Beatmap, BeatmapExt, Mods};
+use akatsuki_pp::{Beatmap as AkatBmap, BeatmapExt};
 use config::CONFIG;
-use sqlx::{
-    mysql::{MySqlPool, MySqlPoolOptions},
-    Executor,
-};
-use std::collections::HashMap;
-use structures::user::User;
+use console::Emoji;
+use indicatif::{HumanDuration, ProgressBar, ProgressStyle};
+use sqlx::{mysql::MySqlPoolOptions, pool, Executor, MySql, MySqlPool, Pool, Row};
+use std::{collections::HashMap, thread, time::Duration};
 use tokio::time::Instant;
 
+use crate::structures::beatmap::Beatmap;
+
 lazy_static::lazy_static! {
+    // id: Beatmap
     pub static ref BMAPCACHE: HashMap<u32, Beatmap> = HashMap::new();
 }
 
+static BEATMAP: Emoji<'_, '_> = Emoji("🎵 ", "");
+static USER: Emoji<'_, '_> = Emoji("👤 ", "");
+static SCORE: Emoji<'_, '_> = Emoji("🏆 ", "");
+static STATS: Emoji<'_, '_> = Emoji("📊 ", "");
+static DONE: Emoji<'_, '_> = Emoji("✅ ", "");
+static FAILED: Emoji<'_, '_> = Emoji("❌ ", "");
+static CONNECTING: Emoji<'_, '_> = Emoji("🔌 ", "");
+static FETCHING: Emoji<'_, '_> = Emoji("📥 ", "");
+
 async fn init_sql() -> Result<MySqlPool, sqlx::Error> {
-    let pool = MySqlPoolOptions::new()
-        .max_connections(10)
-        .connect(
-            &format!(
-                "mysql://{}:{}@{}:{}/{}",
-                CONFIG.MySQL.username,
-                CONFIG.MySQL.password,
-                CONFIG.MySQL.host,
-                CONFIG.MySQL.port,
-                CONFIG.MySQL.database
-            )
-            .as_str(),
-        )
-        .await?;
+    let pb = ProgressBar::new(1);
+    pb.set_style(ProgressStyle::with_template("{prefix:.bold.dim} {wide_msg}").unwrap());
+    pb.set_prefix("[1/5]");
+    pb.set_message(format!("{}Connecting to database...", CONNECTING));
+    let pool_options = MySqlPoolOptions::new().max_connections(10);
+
+    // Construct the MySQL connection URL using configuration values
+    let connection_url = format!(
+        "mysql://{}:{}@{}:{}/{}",
+        CONFIG.MySQL.username,
+        CONFIG.MySQL.password,
+        CONFIG.MySQL.host,
+        CONFIG.MySQL.port,
+        CONFIG.MySQL.database
+    );
+
+    // Connect to the MySQL database using the connection URL
+    let pool_result = pool_options.connect(connection_url.as_str()).await;
+
+    // Handle the connection result
+    let pool = match pool_result {
+        Ok(pool) => pool,
+        Err(error) => {
+            pb.finish_with_message(format!("{}Failed to connect to the database", FAILED));
+            panic!("Failed to connect to the database: {}", error)
+        }
+    };
+
+    pb.finish_with_message(format!("{}Database Connected!", DONE));
+    // delete pb
+    drop(pb);
 
     Ok(pool)
 }
 
-#[tokio::main]
-async fn main() {
-    // Acquire database connection
-    // Create database url from config
-    let pool = init_sql().await.expect("Failed to connect to database");
-
-    // Fetch all users from the database
+async fn relac_maps(pool: &Pool<MySql>, spinner: &ProgressStyle) {
     let start = Instant::now();
-    let users = User::fetch_all(&pool).await;
-    let end = Instant::now();
-    println!("Fetched {} users in {:?}", users.len(), end - start);
+    let maps = Beatmap::fetch_all(&pool).await;
+    let pb = ProgressBar::new(maps.len() as u64);
+    let style = ProgressStyle::default_bar()
+        .progress_chars("█▓▒░ ")
+        .template(
+        "{prefix:.bold.dim} 🎵 Recalculating Beatmaps | {bar:40.white} | {pos}/{len} | ETA: {eta}",
+    );
+    pb.set_style(style.unwrap());
+    pb.set_prefix("[2/5]");
+    pb.set_message(format!("{}Recalculating Beatmaps...", BEATMAP));
+
+    for map in maps {
+        pb.inc(1);
+        thread::sleep(Duration::from_micros(2000));
+    }
+
+    pb.set_style(ProgressStyle::with_template("{prefix:.bold.dim} {wide_msg}").unwrap());
+    pb.set_prefix("[2/5]");
+    pb.finish_with_message(format!(
+        "{}Beatmap recalculation done in {}",
+        DONE,
+        HumanDuration(start.elapsed())
+    ));
 }
 
-// fn recalculate(score: &structures::score::Score, bmap: &Beatmap) {
-//     if (score.mode < 4) {} else if (score.mode in 4..7) {} else if (score.mode == 8) {}
+#[tokio::main]
+async fn main() {
+    // NOTE: That ref passing to unrelated functions is dumb as fuck, change it in future
+    // TODO: Change exception message style to match rest of the code
+    let started = Instant::now();
+    let spinner_style = ProgressStyle::with_template("{prefix:.bold.dim} {spinner} {wide_msg}")
+        .unwrap()
+        .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
 
-//     }
-// }
+    // Connect to the database
+    let pool = init_sql().await.unwrap();
+
+    // Recalc maps
+    relac_maps(&pool, &spinner_style).await;
+}

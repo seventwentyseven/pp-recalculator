00cd2ddce2b64b2deeb03ff653c9706733230b9d
def750 <def75023@gmail.com>
2023-05-17 16:07:12 +0000

    Rename BEATMAPS to BEATMAP_CACHE

--- src/statics.rs
+++ src/statics.rs
@@ -4,6 +4,6 @@ use std::collections::HashMap;
 
 lazy_static::lazy_static! {
     // id: Beatmap
-    pub static ref BEATMAPS: HashMap<u32, Beatmap> = HashMap::new();
+    pub static ref BEATMAP_CACHE: HashMap<u32, Beatmap> = HashMap::new();
     pub static ref HTTPCLIENT: Client = Client::new();
 }
